82d51a2353461d49b8bfaf37a1b60d3065d1f19e
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-29 15:37:29 +0300

    add papka4_1

---
+++ papka4_1/Program.cs
@@ -0,0 +1,30 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 73;
+int b1 = 66;
+int c1 = 666;
+int a2 = 43;
+int b2 = 88;
+int c2 = 99832;
+int a3 = 61;
+int b3 = 99999;
+int c3 = 70;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max1);
+Console.WriteLine(max2);
+Console.WriteLine(max3);
+
+Console.Write("Максимальное из всех чисел - ");
+Console.WriteLine(max);
+
