90fc3f7ec4e1ea3ae1f2482393d663ad6c4528e6
otbornyi <94071821+otbornyi@users.noreply.github.com>
2021-11-29 16:20:32 +0300

    add papka4_3

---
+++ papka4_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = {12, 73, 11, 83, 85, 93};
+int n = array.Length;
+int find = 85;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    Console.WriteLine(index);
+    index++;
+}
